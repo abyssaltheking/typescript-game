dbbb73db2d1f8341475356e7b3b3dd8fc58ab346
abyssaltheking <gamerlol5epic@gmail.com>
2025-07-15 18:09:09 -0400

    prettier

--- src/helperMath.ts
+++ src/helperMath.ts
@@ -1,22 +1,30 @@
 export class HelperMath {
-    static clamp(value: number, minimumOutput: number, maximumOutput: number): number {
-        let newOutput = value;
+	static clamp(
+		value: number,
+		minimumOutput: number,
+		maximumOutput: number,
+	): number {
+		let newOutput = value;
 
-        if (value < minimumOutput) newOutput = minimumOutput;
-        if (value > maximumOutput) newOutput = maximumOutput;
-    
-        return newOutput;
-    }
+		if (value < minimumOutput) newOutput = minimumOutput;
+		if (value > maximumOutput) newOutput = maximumOutput;
 
-    static isValueInRange(value: number, minimum: number, maximum: number): boolean {
-        return minimum <= value && value <= maximum;
-    }
+		return newOutput;
+	}
 
-    static getRandomNumber(minimum: number, maximum: number): number {
-        return HelperMath.clamp(Math.random() * maximum, minimum, maximum);
-    }
+	static isValueInRange(
+		value: number,
+		minimum: number,
+		maximum: number,
+	): boolean {
+		return minimum <= value && value <= maximum;
+	}
 
-    static degreesToRadians(value: number): number {
-        return value * Math.PI / 180;
-    }
+	static getRandomNumber(minimum: number, maximum: number): number {
+		return HelperMath.clamp(Math.random() * maximum, minimum, maximum);
+	}
+
+	static degreesToRadians(value: number): number {
+		return (value * Math.PI) / 180;
+	}
 }
